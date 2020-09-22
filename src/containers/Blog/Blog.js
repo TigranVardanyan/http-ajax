153837fa4bbd4bfbbad9560cf8f3adf7bb4d6b57
Tigran <tig.vardanyan.001@gmail.com>
2020-09-22 04:49:51 +0400

    Handling Errors Locally

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -10,9 +10,11 @@ class Blog extends Component {
   state = {
     posts: [],
     selectedPostId: null,
+    error: false,
+
   }
   componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
+    axios.get('https://jsonplaceholder.typicode.com/postsss')
       .then(response => {
         const posts = response.data.slice(0, 4);
         const updatedPosts = posts.map(
@@ -26,6 +28,9 @@ class Blog extends Component {
         this.setState({posts:updatedPosts});
         //console.log(response)
       })
+      .catch(error => {
+        this.setState({error:true})
+      })
   }
 
   postSelectedHandler = (id) => {
@@ -33,16 +38,20 @@ class Blog extends Component {
   }
 
   render () {
-    const posts = this.state.posts.map(
-      post => {
-        return <Post
-          key={post.id}
-          title={post.title}
-          author={post.author}
-          clicked={() => this.postSelectedHandler(post.id)}
-        />
-      }
-    )
+    let posts = <p style={{textAlign:'center'}}>Somthing went wrong</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map(
+        post => {
+          return <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
+        }
+      )
+    }
+
         return (
             <div>
                 <section className="Posts">
