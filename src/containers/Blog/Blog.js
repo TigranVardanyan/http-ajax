7cb0653aa6323764a37945de39e991e96c26a75a
Tigran <tig.vardanyan.001@gmail.com>
2020-09-23 05:05:33 +0400

    Extracting Route Parameters

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {NavLink, Route} from "react-router-dom";
+import {NavLink, Route, Switch} from "react-router-dom";
 import './Blog.css';
 import Posts from './Posts/Posts'
 import NewPost from './NewPost/NewPost';
