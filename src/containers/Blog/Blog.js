a13e14106667f1825c279d9689ac9b80efc7ef4b
Tigran <tig.vardanyan.001@gmail.com>
2020-09-23 04:02:20 +0400

    Preparing the Project For Routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,52 +2,17 @@ import React, { Component } from 'react';
 import axios from '../../axios';
 
 import './Blog.css';
+import Posts from './Posts/Posts'
+
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null,
-    error: false,
 
-  }
-  componentDidMount() {
-    axios.get('/posts')
-      .then(response => {
-        const posts = response.data.slice(0, 4);
-        const updatedPosts = posts.map(
-          post => {
-            return {
-              ...post,
-              author: "Max"
-            }
-          }
-        )
-        this.setState({posts:updatedPosts});
-        //console.log(response)
-      })
-      .catch(error => {
-        this.setState({error:true})
-      })
-  }
 
-  postSelectedHandler = (id) => {
-    this.setState({selectedPostId:id})
-  }
+
+
 
   render () {
-    let posts = <p style={{textAlign:'center'}}>Somthing went wrong</p>;
-    if (!this.state.error) {
-      posts = this.state.posts.map(
-        post => {
-          return <Post
-            key={post.id}
-            title={post.title}
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)}
-          />
-        }
-      )
-    }
+
 
         return (
             <div className={"Blog"}>
@@ -61,9 +26,7 @@ class Blog extends Component {
                   </li>
                 </ul>
               </header>
-                <section className="Posts">
-                  {posts}
-                </section>
+                <Posts/>
                 {/*<section>*/}
                 {/*    <FullPost id={this.state.selectedPostId}/>*/}
                 {/*</section>*/}
